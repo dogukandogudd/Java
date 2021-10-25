40142dd94b33ac705f1151251a0a16c242da9fbe
Doğukan <dogukandogu8@gmail.com>
2021-10-25 18:04:28 +0300

    Java Dersi 28

--- Java Projects/classes/src/classes/Main.java
+++ Java Projects/classes/src/classes/Main.java
@@ -1,8 +1,14 @@
+package classes;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		CustomerManager customerManager = new CustomerManager();
+		CustomerManager customerManager2 = new CustomerManager();
+		customerManager=customerManager2;
+		customerManager.Add();
+		customerManager.Remove();
+		customerManager.Update();
 
 	}
 
